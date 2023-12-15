79d9145d2e0225953a2d38c19028d23d650efd8f
Kavyapuli <pulikavya10@gmail.com>
2023-12-15 20:22:21 +0530

    committing

---
+++ RainbowStudentData/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace RainbowStudentData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string filePath = "C:/Users/Dell/Desktop/ProjDir2/RainbowStudentData/RainbowStudentData/Data/studentdata.txt";
+          if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File not found: " + filePath);
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            Console.WriteLine("Student Data:");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
